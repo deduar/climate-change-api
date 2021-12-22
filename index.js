200a308a9789160bb9762e718f5a134d40ae881a
Eduardo Diez <deduar@gmail.com>
2021-12-22 11:58:46 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const PORT = 8000;
+const express = require('express');
+const axios = require('axios');
+const cherrio = require('cheerio');
+const { response } = require('express');
+
+const app = express();
+
+const newspapers = [
+    {
+        name: 'thetimes',
+        address: 'https://www.thetimes.co.uk/environment/climate-change'
+    },
+    {
+        name: 'guardian',
+        address: 'https://www.theguardian.com/environment/climate-crisis'
+    },
+    {
+        name: 'telegraph',
+        address: 'https://www.telegraph.co.uk/climate-change'
+    }
+]
+const articles = []
+
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+        .then(response => {
+            const html = response.data
+            const $ = cherrio.load(html)
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                articles.push({
+                    title,
+                    url,
+                    source: newspaper.name
+                })
+            })
+        })
+});
+
+app.get('/',(req,res) => {
+    res.json('Welcome to my Climate Change News API')
+});
+
+app.get('/news',(req,res) => {
+    res.json(articles)
+});
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
